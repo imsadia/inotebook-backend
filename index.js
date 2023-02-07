b185bd60a660a6ed76640a061f1136938fadf907
imsadia <maliksadia0015@gmail.com>
2023-02-07 12:08:47 +0500

    Basic user creation

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const express = require('express');
 connectToMongo();
 
 const app = express();
-const port = 3000;
+const port = 3000; 
+
+
+app.use(express.json());
 
 // Available Routes of the Application
 app.use('/api/auth', require('./routes/auth'));
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const User = require('../models/User');
 const router = express.Router();
 
+
+// Create a User using: POST "/api/auth/". Doesn't Require Auth.
 router.get('/', (req, res) => {
-    res.json([]);
+    const user = User(req.body);
+    user.save();
+    res.json(req.body);
 });
 
 module.exports = router;
